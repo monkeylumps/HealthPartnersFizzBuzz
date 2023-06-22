00a3e41a2f6b9236b10469845d436f107727e51f
dan <d>
2023-06-22 17:59:10 +0100

    added rule for buzz

---
+++ FizzBuzzService/BuzzRule.cs
@@ -0,0 +1,15 @@
+﻿namespace FizzBuzzService
+{
+    internal class BuzzRule : IFizzBuzzRule
+    {
+        public bool IsMatch(int number)
+        {
+            return number % 5 == 0;
+        }
+
+        public string GetOutput()
+        {
+            return "Buzz";
+        }
+    }
+}
