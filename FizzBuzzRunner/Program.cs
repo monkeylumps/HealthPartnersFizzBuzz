69f4be5c7073a5129333eb97989a7df07454170d
dan <d>
2023-06-22 17:14:21 +0100

    initial commit

---
+++ FizzBuzzRunner/Program.cs
@@ -0,0 +1,11 @@
+﻿//“To create a solution to the Fizz Buzz problem, using C#, that is built using Reflection and which
+//demonstrates a clean, Test Driven approach using SOLID design principles.”
+//Again the problem you are looking to solve is:
+//“Given a range of numbers, loop through each number and output the following:
+// If the number is divisible by 3 then output the word Fizz instead of the number.
+// If the number is divisible by 5 then output the word Buzz instead of the number.
+// If the number is divisible by both 3 and 5 then output the word Fizzbuzz instead of the
+//number.
+// If none of the above conditions are met then output the number.
+
+Console.WriteLine("Hello, World!");
